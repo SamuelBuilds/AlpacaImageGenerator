2a91ad6d267c729d2be150eceb245464dd40239e
SamuelBuilds <babuasamuel@gmail.com>
2024-09-25 09:20:52 +0300

    worked on most of the functionality

--- src/App.js
+++ src/App.js
@@ -2,7 +2,10 @@ import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  //Show Div with buttons
+  const [isVisible, setIsVisible] = useState(true);
   const [data,setData]=useState([]);
+  const [selectedCategory, setSelectedCategory] = useState(null);
   //Get that sweet data.
   const getData = () =>{
       fetch(
@@ -25,41 +28,67 @@ function App() {
     useEffect(()=>{
       getData()
     },[]);
+    const toggleVisibility = (category) => {
+      setIsVisible(isVisible);
+      setSelectedCategory(category);
+    };
+    const [imageUrls, setImageUrls] = useState({
+      backgrounds: 'https://res.cloudinary.com/dijtpwbyc/image/upload/v1727023126/yellow70_us2bkc.png',
+      neck: 'https://res.cloudinary.com/dijtpwbyc/image/upload/v1727023113/default_vl7wow.png',
+      leg: "https://res.cloudinary.com/dijtpwbyc/image/upload/v1727023107/default_vidpk3.png",
+      mouth: "https://res.cloudinary.com/dijtpwbyc/image/upload/v1727023110/default_skjval.png",
+      nose: 'https://res.cloudinary.com/dijtpwbyc/image/upload/v1727023115/nose_ils0np.png',
+      ears: "https://res.cloudinary.com/dijtpwbyc/image/upload/v1727023127/default_hxsdp7.png",
+      accessories: "https://res.cloudinary.com/dijtpwbyc/image/upload/v1727023060/headphone_hhcuwh.png",
+      hair: "https://res.cloudinary.com/dijtpwbyc/image/upload/v1727023103/default_z6mxwh.png",
+      eyes: "https://res.cloudinary.com/dijtpwbyc/image/upload/v1727023098/default_j4kidi.png",
+    }); 
+    const handleButtonClick = (category,item) => {
+      setImageUrls(
+        (prevUrls)=>({
+          ...prevUrls,
+          [category]: data[category][item],
+        })
+      )
+    }
+    useEffect(() => {
+      console.log('Initial image URLs:', imageUrls);
+    }, [imageUrls]);
   return (
     <div className="App">
       <div className="content">
         <div className="title">Alpaca Image Generator</div>
         <div className="editor">
-        <img src={data.backgrounds?.yellow70} className="layer"></img>
-        <img src={data.neck?.default} className="layer"></img>
-        <img src={data.leg?.default} className="layer"></img>
-        <img src={data.mouth?.default} className="layer"></img>
-        <img src={data.nose?.nose} className="layer"></img>
-        <img src={data.ears?.default} className="layer"></img>
-        <img src={data.accessories?.headphone} className="layer"></img>
-        <img src={data.hair?.default} className="layer"></img>
-        <img src={data.eyes?.default} className="layer"></img>
+            <img src={imageUrls.backgrounds} className="layer" alt="background" ></img>
+            <img src={imageUrls.neck} className="layer" alt="neck" ></img>
+            <img src={imageUrls.leg} className="layer" alt="leg" ></img>
+            <img src={imageUrls.mouth} className="layer" alt="mouth" ></img>
+            <img src={imageUrls.nose} className="layer" alt="nose" ></img>
+            <img src={imageUrls.ears} className="layer" alt="ears" ></img>
+            <img src={imageUrls.accessories} className="layer" alt="accessories" ></img>
+            <img src={imageUrls.hair} className="layer" alt="hair" ></img>
+            <img src={imageUrls.eyes} className="layer" alt="eyes" ></img>
         </div>
         <div className="items">
           <div><p>buttons here</p></div>
           <div className='buttons'>
           {Object.keys(data).map((category) => (
-        <button key={category}>
+        <button key={category} onClick={() => toggleVisibility(category)}>
+          
           {category.charAt(0).toUpperCase() + category.slice(1)}
         </button>
       ))}
           </div>
           </div>
+          {isVisible && selectedCategory && (
         <div className="options">
           <div><p>buttons here</p></div>
           <div className='buttons'>
-            <button>hello</button>
-            <button>hello</button>
-            <button>hello</button>
-            <button>hello</button>
-            <button>hello</button>
+          {Object.keys(data[selectedCategory]).map((item, index) => (
+              <button key={index} onClick={() => handleButtonClick(selectedCategory, item)}>{item}</button>
+            ))}
           </div>
-        </div>
+        </div>)}
         <div className="optional">not Again</div>
     </div>
     </div>
